2666a11ff033116f0a1d1e0ce65c6064d7e66e50
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2023-03-04 23:36:34 +0100

    alles

---
+++ Desktop/software developer/leer routes/programmering/leren programmeren/Dynamische User Interfaces/m01o1/m01o1.js
@@ -0,0 +1,15 @@
+let uitleg = "In een parkeergarage passen 80 auto's op de begane grond en 120 op een verdieping.\n"+
+"Vraag het gewenste aantal auto's in de garage en bereken het aantal verdiepingen wat je nodig hebt.";
+
+alert(uitleg);
+let gewenste_aantal_autos = prompt("Hoeveel autos wilt u kwijt in de parkeergarage?");
+// Berekenen hier het aantal verdiepingen
+let rekening = Math.ceil((gewenste_aantal_autos - 80) / 120); 
+document.getElementById("rekening").innerText = rekening;
+console.log(rekening)
+window.alert(rekening)
+
+let antwoord = "Ik heb nog geen idee hoeveel verdiepingen er moeten komen :-( ";
+document.getElementById("rekening").innerText = rekening;
+
+console.log("Om het gewenste aantal autos kwijt te kunnen heb ik x verdiepingen nodig.")
