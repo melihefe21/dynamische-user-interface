7e90174a8af0582d94180a0a420e0a833254ca5c
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2023-03-09 13:24:35 +0100

    horeca

---
+++ Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/help.js
@@ -0,0 +1,5 @@
+didjoshsleepwithyourmomtonight = false
+didjoshsleepwithyourmomtonight === true ? console.warn("fatherless?") : console.warn("your mother got no bitches")
+statement ? true : false
+# or = ||
+# and = &&
---
+++ Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/horeca/horeca.js
@@ -0,0 +1,8 @@
+let horeca = {"bier":2.50,"fris":1.50,"wijn":5.50};
+bon = {}
+userinput = prompt("wat wilt u?")
+if (userinput) not in horeca;
+    print("dat hebben wij niet?")
+    else if(userinput) === horeca;
+        prompt("hoeveel?")
+
