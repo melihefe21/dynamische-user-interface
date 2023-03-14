a240e0fc84edfd8ad55c8b8eb6b837fc18dca8af
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2023-03-14 11:14:20 +0100

    horeca

--- Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/help.js
+++
@@ -1,5 +0,0 @@
-didjoshsleepwithyourmomtonight = false
-didjoshsleepwithyourmomtonight === true ? console.warn("fatherless?") : console.warn("your mother got no bitches")
-statement ? true : false
-# or = ||
-# and = &&
--- Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/horeca/horeca.js
+++ Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/horeca/horeca.js
@@ -1,8 +1,50 @@
-let horeca = {"bier":2.50,"fris":1.50,"wijn":5.50};
-bon = {}
-userinput = prompt("wat wilt u?")
-if (userinput) not in horeca;
-    print("dat hebben wij niet?")
-    else if(userinput) === horeca;
-        prompt("hoeveel?")
+const dranken = {"bier": 3.50, "fris": 3.20 , "wijn": 4.75};
+const bonnetje = {};
 
+let totale_prijs = 0;
+let stop = false;
+let userInput;
+let productAmount = NaN;
+let amountNaN = true;
+
+while (!stop) {
+    userInput = prompt(`Wat zou u willen bestellen? (Type 'stop' om de bestelling te beëndigen.) Deze dranken zijn op voorraad: ${Object.keys(dranken)}`);
+
+    if (userInput === "stop") {
+        stop = true;
+    }
+    else {
+        if (!(userInput in dranken)) {
+            alert(`Momenteel is ${userInput} uitverkocht. Sorry voor dit ongemak.`);
+        }
+        else {
+            while (amountNaN) {
+                productAmount = parseInt(prompt(`Hoeveel ${userInput} wilt u bestellen?`));
+                if (!isNaN(productAmount)) {
+                    amountNaN = false;
+                }
+            }
+
+            amountNaN = true;
+
+            if (userInput in bonnetje) {
+                bonnetje[userInput] += productAmount;
+            }
+            else {
+                bonnetje[userInput] = productAmount;
+            }
+
+            totale_prijs += productAmount * dranken[userInput];
+        }
+    }
+}
+
+for (const [key, value] of Object.entries(bonnetje)) {
+    const display = `${value}x ${key} - €${(value * dranken[key]).toFixed(2)}`
+    document.getElementById("Bonnetje").innerText = "Uw Bonnetje - Bedankt voor het bestellen!";
+    document.getElementById("Dranken").innerText += display + '\n';
+}
+
+if (totale_prijs > 0) {
+    document.getElementById("Totale_prijs").innerText = "Totaal: €" + totale_prijs.toFixed(2);
+}
