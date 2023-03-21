94c11777313708bb81c666d7a3740dab5594cf13
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2023-03-21 12:07:59 +0100

    alesssssssssss

--- Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/horeca/horeca.js
+++ Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/horeca/horeca.js
@@ -1,50 +1,7 @@
-const dranken = {"bier": 3.50, "fris": 3.20 , "wijn": 4.75};
-const bonnetje = {};
+let wat = prompt("wat wilt u?");
+let hoeveel = prompt("hoeveel wilt u?")
+let bestelling = {bier : 0, Wijn: 0, fris: 0};
+bestelling['wat'] += hoeveel
 
-let totale_prijs = 0;
-let stop = false;
-let userInput;
-let productAmount = NaN;
-let amountNaN = true;
-
-while (!stop) {
-    userInput = prompt(`Wat zou u willen bestellen? (Type 'stop' om de bestelling te beëndigen.) Deze dranken zijn op voorraad: ${Object.keys(dranken)}`);
-
-    if (userInput === "stop") {
-        stop = true;
-    }
-    else {
-        if (!(userInput in dranken)) {
-            alert(`Momenteel is ${userInput} uitverkocht. Sorry voor dit ongemak.`);
-        }
-        else {
-            while (amountNaN) {
-                productAmount = parseInt(prompt(`Hoeveel ${userInput} wilt u bestellen?`));
-                if (!isNaN(productAmount)) {
-                    amountNaN = false;
-                }
-            }
-
-            amountNaN = true;
-
-            if (userInput in bonnetje) {
-                bonnetje[userInput] += productAmount;
-            }
-            else {
-                bonnetje[userInput] = productAmount;
-            }
-
-            totale_prijs += productAmount * dranken[userInput];
-        }
-    }
-}
-
-for (const [key, value] of Object.entries(bonnetje)) {
-    const display = `${value}x ${key} - €${(value * dranken[key]).toFixed(2)}`
-    document.getElementById("Bonnetje").innerText = "Uw Bonnetje - Bedankt voor het bestellen!";
-    document.getElementById("Dranken").innerText += display + '\n';
-}
-
-if (totale_prijs > 0) {
-    document.getElementById("Totale_prijs").innerText = "Totaal: €" + totale_prijs.toFixed(2);
-}
+document.write("<br>")
+document.write('bier : ' + bestelling)
--- Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/ruitjes/ruitjes.js
+++ Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/ruitjes/ruitjes.js
@@ -1,11 +1,29 @@
-let cijfer = prompt("Voer een cijfer in voor de ruit");
-let lijstje = "";
-for (let getallen = 1; getallen <= cijfer; getallen++){
-    lijstje += getallen;
-    document.getElementById("getal").innerText += lijstje +'\n';
-    lijstje = getallen === parseInt(cijfer) ? lijstje += "" : lijstje += "-";
+let nummer = prompt("geef een getal: ")
+
+let getal = 0
+
+for(let i = 0; i< nummer; i++) {
+    for(let z = 0; z < i+1; z++){
+        if(z == getal){
+            document.write(z + 1)
+        }
+        else{
+            document.write(z+ 1 + "-" )
+        }
+        
+    }
+    getal+= 1
+    document.write("<br>")
 }
-for (let getallen = cijfer; getallen >= 1; getallen--) {
-    lijstje = lijstje.replace(getallen, '').split("").reverse().join("").replace('-', '').split("").reverse().join("");
-    document.getElementById("getal").innerText += lijstje + '\n';
+getal = 19
+for(i = nummer; i > 0; i--) {
+    for(let z = 1; z < i; z++){
+        if(z == getal){
+            document.write(z) 
+        } else{
+            document.write(z + "-")
+        }
+    }
+    getal-=1
+    document.write("<br>")
 }
