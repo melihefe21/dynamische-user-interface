ba18f140e16ba5af92af0df1ee7465f78414d0b5
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2023-03-26 15:07:47 +0200

    vergeten

--- Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/horeca/horeca.js
+++ Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/horeca/horeca.js
@@ -1,7 +1,52 @@
-let wat = prompt("wat wilt u?");
-let hoeveel = prompt("hoeveel wilt u?")
-let bestelling = {bier : 0, Wijn: 0, fris: 0};
-bestelling['wat'] += hoeveel
+const fris = 2.00;
+const bier = 2.50;
+const wijn = 3.00;
+var bonnetje = [];
 
-document.write("<br>")
-document.write('bier : ' + bestelling)
+while (true) {
+    let product = prompt("Welk product wil je toevoegen? (of typ 'stop' om te stoppen)");
+    if (product == "stop") {
+        break;
+    } else {
+        if (product !== "fris" && product !== "bier" && product !== "wijn") {
+            alert("Dit ken ik niet.");
+        } else {
+            let aantal = prompt("Hoeveel " + product + " wil je hebben?");
+            let gevonden = false;
+            for (var i = 0; i < bonnetje.length; i++) {
+                if (bonnetje[i].product == product) {
+                    bonnetje[i].aantal = parseInt(bonnetje[i].aantal) + parseInt(aantal);
+                    gevonden = true;
+                    break;
+                }
+            }
+            if (!gevonden) {
+                bonnetje.push({ product: product, aantal: aantal });
+            }
+            alert(aantal + " " + product + " is toegevoegd aan het bonnetje.");
+        }
+    }
+}
+if (bonnetje.length > 0) {
+    var bonnetjeOutput = "Bonnetje:<br>";
+    var totaalprijs = 0;
+    for (var i = 0; i < bonnetje.length; i++) {
+        var product = bonnetje[i].product;
+        var aantal = bonnetje[i].aantal;
+        var prijs = 0;
+        if (product === "fris") {
+            prijs = fris;
+        }
+        else if (product === "bier") {
+            prijs = bier;
+        } 
+        else if (product === "wijn") {
+            prijs = wijn;
+        }
+        var productTotaal = prijs * aantal;
+        bonnetjeOutput += aantal + " " + product + " " + prijs.toFixed(2)  + " " + productTotaal.toFixed(2) + " ";
+        totaalprijs += productTotaal;
+    }
+    bonnetjeOutput += "Totaal: " + " " + totaalprijs.toFixed(2) + " " + " euro";
+    document.getElementById("output").innerHTML = bonnetjeOutput;
+}
