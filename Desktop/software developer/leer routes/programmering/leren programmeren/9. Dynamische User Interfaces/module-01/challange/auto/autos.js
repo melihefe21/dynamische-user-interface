922656c93d906d64e2d377ccc977a1a1fc2ce9b2
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2023-04-11 23:33:08 +0200

    iets

---
+++ Desktop/software developer/leer routes/programmering/leren programmeren/9. Dynamische User Interfaces/module-01/challange/auto/autos.js
@@ -0,0 +1,81 @@
+let cars = [
+    "Audi A3",
+    "BMW 3 Series",
+    "Chevrolet Camaro",
+    "Dodge Charger",
+    "Ford Mustang",
+    "Honda Civic",
+    "Infiniti Q50",
+    "Jaguar F-Type",
+    "Kia Optima",
+    "Lamborghini Huracan",
+    "Mercedes-Benz C-Class",
+    "Ferrari F8 Tributo",
+    "Nissan Altima",
+    "Porsche 911",
+    "Range Rover Evoque",
+    "Subaru Impreza",
+    "Ferrari SF90 Stradale",
+    "Tesla Model S",
+    "Toyota Corolla",
+    "Volkswagen Golf",
+    "Acura NSX",
+    "Bentley Continental",
+    "Cadillac Escalade",
+    "Ferrari 488 GTB",
+    "Dodge Challenger",
+    "Ferrari 458",
+    "GMC Sierra",
+    "Hyundai Elantra",
+    "Jeep Grand Cherokee",
+    "Ferrari SF90 Stradale",
+    "Koenigsegg Agera",
+    "Lexus LS",
+    "Maserati GranTurismo",
+    "Nissan GT-R",
+    "Pagani Huayra",
+    "Rolls-Royce Ghost",
+    "Smart ForTwo",
+    "Tesla Model X",
+    "Toyota Prius",
+    "Volkswagen Jetta",
+    "Alfa Romeo Giulia",
+    "Bugatti Chiron",
+    "Chevrolet Corvette",
+    "Ferrari Portofino",
+    "Fiat 500",
+    "GMC Yukon",
+    "Honda Accord",
+    "Jaguar XJ",
+    "Kia Soul",
+    "Lamborghini Aventador",
+    "Mercedes-Benz E-Class",
+    "Nissan Maxima",
+    "Porsche Panamera",
+    "Range Rover Sport",
+    "Subaru Legacy",
+    "Tesla Model 3",
+    "Toyota Camry",
+    "Volkswagen Passat"
+    ];
+     
+    console.log(cars[20]);
+    cars.sort();
+
+
+    let ul = document.createElement("ul");
+    ul.setAttribute("id", "lijst");
+    document.body.appendChild(ul);
+for (i=0; i<cars.length; i++){
+    lijst = document.getElementById("lijst");
+    let li = document.createElement("li");
+    li.innerHTML = cars[i];
+    if (cars[i].includes("Ferrari")){
+        li.setAttribute("class", "red");
+    }
+    lijst.appendChild(li)
+    
+    
+
+}
+    
